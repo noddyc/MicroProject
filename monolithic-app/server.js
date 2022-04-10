cfee83efe6de3b146a8bfec82783c27560129461
jianhe2git <59978308+noddyc@users.noreply.github.com>
2022-04-10 14:46:39 -0400

    1

--- monolithic-app/server.js
+++ monolithic-app/server.js
@@ -58,8 +58,12 @@ app.get('/api/newservice', function(req, res){
     res.send(db.newservice);
  });
  
- app.get('/api/newservice2', function(req, res){
-    res.send(db.newservice2);
- });
+app.get('/api/newservice2', function(req, res){
+res.send(db.newservice2);
+});
+
+app.get('/api/newservice3', function(req, res){
+res.send(db.newservice3);
+});
 
 app.listen(3000);
